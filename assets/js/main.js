5f5e82437ba1bfeec5d17ff65a59e8f678c136ee
André Bueno <andrebueno99@gmail.com>
2023-05-14 14:08:13 -0300

    Cores, botões e dados funcionando. FINAL

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,62 @@
 console.log("oi");
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
- 
-
-function convertPokemonTypesToLi(pokemonTypes)
-{
-    return pokemonTypes.map((typeSlot) => {
-        return `<li class="type">${typeSlot.type.name}</li>`
-    })
-}
+let offset = 0;
+const limit = 5;
+const maxRecords = 151
 
 
-
-function convertPokemonToLi(pokemon) {
-    return `
-    <li class="pokemon">
-        <span class="number">${pokemon.order}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                ${convertPokemonTypesToLi(pokemon.types).join('')} 
-            </ol>
-            <img
-                src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-        </div>
-    </li>
-    `
-}
-
+const loadMoreButton = document.getElementById("loadMore");
 const pokemonList = document.getElementById('pokemonList')
 
 
-pokeApi.getPokemons().then((pokemons = []) => {
+function loadPokemonItens(offset, limit) {
 
-    const newList = pokemons.map((pokemon) => {
-        return convertPokemonToLi(pokemon)
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+
+        const newHTML = pokemons.map((pokemon) => {
+            return `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class='type ${type}'>${type}</li>`).join("")}
+                    </ol>
+                    <img
+                        src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+            `
+        }).join('')
+
+        pokemonList.innerHTML += newHTML
+
+
+
+
     })
 
-    const newHTML = newList.join('');
-    pokemonList.innerHTML += newHTML
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+
+    offset += limit
+
+    const qtdRecordNextPage = offset + limit;
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
 
 
-
-    /*
-     const listItems = []
-
-
-     for (let i = 0; i < pokemons.length; i++) {
-         const pokemon = pokemons[i];
-         listItems.push(convertPokemonToLi(pokemon));
-         
-
-     }
-     console.log(listItems)
-     */
 })
-
-
-    .catch((error) => console.log(error));
-
 /** 
     .then(function (response) {
         return response.json();
@@ -78,3 +74,16 @@ pokeApi.getPokemons().then((pokemons = []) => {
         console.log("Requisição feita.")
     })
 */
+
+/*
+    const listItems = []
+
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon));
+        
+
+    }
+    console.log(listItems)
+    */
